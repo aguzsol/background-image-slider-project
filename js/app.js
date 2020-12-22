e425ad693c2cdefd0efa3ab385b2813e6d10e19c
albaguz <aguzmansolana@gmail.com>
2020-12-22 11:23:43 +0100

    solution-alba

--- js/app.js
+++ js/app.js
@@ -5,3 +5,35 @@
     "contBcg-3",
     "contBcg-4"
   ];
+
+
+const buttonLeft = document.querySelector('.btn-left')
+const buttonRight = document.querySelector('.btn-right')
+var change = document.querySelector('.img-container')
+var counter=0;
+
+console.log(buttonRight)
+console.log(buttonLeft)
+console.log(change)
+
+function nextImg(e) {
+  if (e.target.id == 'btn-right'){
+    counter++
+    if(counter > pictures.length-1){
+      counter=0
+    }
+  }
+  else{
+    if(counter <= 0){
+      counter=4
+    }
+    else{
+      counter--
+    }
+  }
+  change.style.backgroundImage = "url(../img/"+ pictures[counter] + ".jpeg)"
+  console.log(counter)
+}
+
+buttonLeft.addEventListener('click', nextImg)
+buttonRight.addEventListener('click', nextImg)
