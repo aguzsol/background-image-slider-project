92d2968cdfac7241178f33c199bb40a62c6362e4
albaguz <aguzmansolana@gmail.com>
2020-12-23 10:14:24 +0100

    update-event.target-por-event.currentTarget-bubblingIssue

--- js/app.js
+++ js/app.js
@@ -1,37 +1,37 @@
-  const pictures = [
-    "contBcg-0",
-    "contBcg-1",
-    "contBcg-2",
-    "contBcg-3",
-    "contBcg-4"
-  ];
+const pictures = [
+  "contBcg-0",
+  "contBcg-1",
+  "contBcg-2",
+  "contBcg-3",
+  "contBcg-4"
+];
 
 
 const buttonLeft = document.querySelector('.btn-left')
 const buttonRight = document.querySelector('.btn-right')
 var change = document.querySelector('.img-container')
-var counter=0;
+var counter = 0;
 
 console.log(buttonRight)
 console.log(buttonLeft)
 console.log(change)
 
 function nextImg(e) {
-  if (e.target.id == 'btn-right'){
+  if (e.currentTarget.id == 'btn-right') {
     counter++
-    if(counter > pictures.length-1){
-      counter=0
+    if (counter > pictures.length - 1) {
+      counter = 0
     }
   }
-  else{
-    if(counter <= 0){
-      counter=4
+  else {
+    if (counter <= 0) {
+      counter = 4
     }
-    else{
+    else {
       counter--
     }
   }
-  change.style.backgroundImage = "url(../img/"+ pictures[counter] + ".jpeg)"
+  change.style.backgroundImage = "url(../img/" + pictures[counter] + ".jpeg)"
   console.log(counter)
 }
 
